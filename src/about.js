c64447871638c0c8a9099979181aef8561b5243b
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-03-08 14:33:51 +0800

    Create about.js

---
+++ src/about.js
@@ -0,0 +1,30 @@
+import React from 'react';
+ 
+function About() {
+  return (
+    <form id="about">
+	    <h1>Roland Christian Too Regacho</h1>
+	    <h3 className="title"><a>BIRTHDAY</a></h3>
+	    <h3>February 11, 1998</h3>
+	    
+	    <h3 className="title"><a>ADDRESS</a></h3>
+	    <h3>0558 Gov Padilla Rd Agnaya, Plaridel, Bulacan</h3>
+	    
+	    <h2>EDUCATION</h2>
+	    <h3 className="title"><a>TERTIARY</a></h3>
+	    <h3>Bulacan State University(2014-2019)
+	    <i>BS Information Technology</i></h3>
+	    
+	    <h3 className="title"><a>SECONDARY</a></h3>
+	    <h3>Rafaela Academy Inc.(2010-2014)</h3>
+	    
+	    <h3 className="title"><a>PRIMARY</a></h3>
+	    <h3>Rafaela Academy Inc.(2004-2010)</h3>
+	    
+	    <h2>ABOUT</h2>
+	    <h4>An IT Graduate with exceptional strengths in handling critical problem solving activities. I also love playing PC games mostly MOBA games which includes Dota 2 and League of Legends, I also play a little bit of RPG and FPS games. I also love developing small web applications to test my skills in web development. I will absolutely love landing a job in a company that can help me grow as a professional and promotes a good life and work balance.</h4>
+	</form>
+  )
+}
+ 
+export default About;
