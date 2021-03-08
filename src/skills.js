25356e16781f686f56871889b5e3924b598e5762
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-03-08 14:33:15 +0800

    Create skills.js

---
+++ src/skills.js
@@ -0,0 +1,48 @@
+import React from 'react';
+ 
+function Skills() {
+  return (
+	<form id="skills">
+		<table id='skillsTable'>
+			<tr>
+				<td class="skillType"><b>PROGRAMMING SKILL</b></td>
+			</tr>
+			<tr>
+				<td class="skillTitle">PHP</td>
+			</tr>
+			<tr>
+				<td class="skillTitle">Javascript</td>
+			</tr>
+			<tr>
+				<td class="skillTitle">Css</td>
+			</tr>
+			<tr>
+				<td class="skillTitle">Python</td>
+			</tr>
+			<tr>
+				<td class="skillTitle">Java</td>
+			</tr>
+			<tr>
+				<td class="skillType"><b>PERSONAL SKILL</b></td>
+			</tr>
+			<tr>
+				<td class="skillTitle">Communication Skills</td>
+			</tr>
+			<tr>
+				<td class="skillTitle">Ability to work Individually</td>
+			</tr>
+			<tr>
+				<td class="skillTitle">Ability to work as part of a team</td>
+			</tr>
+			<tr>
+				<td class="skillTitle">Critical Thinking Skills</td>
+			</tr>
+			<tr>
+				<td class="skillTitle">Decision Making Skills</td>
+			</tr>
+		</table>
+	</form>
+  )
+}
+ 
+export default Skills;
