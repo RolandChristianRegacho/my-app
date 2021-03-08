3652b95ab2007abc23de3869f8cb1ea962b1ccb3
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-03-08 14:33:38 +0800

    Create contact.js

---
+++ src/contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+ 
+function Contact() {
+  return (
+	<form id="contact">
+		<h3>Gmail</h3>
+		<h4>regacho800@gmail.com</h4>
+		
+		<h3>Phone Number</h3>
+		<h4>+639215382233</h4>
+		
+		<h3>Skype</h3>
+		<h4>live:.cid.8691244394ae269b</h4>
+		
+		<h3>Viber</h3>
+		<h4>+639215382233</h4>
+	</form>
+  )
+}
+ 
+export default Contact;
