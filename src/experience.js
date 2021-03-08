557778269175a6aa78d0b41d9748bc770f107bce
naseGaming <75259393+naseGaming@users.noreply.github.com>
2021-03-08 14:33:34 +0800

    Create experience.js

---
+++ src/experience.js
@@ -0,0 +1,24 @@
+import React from 'react';
+ 
+function Experience() {
+  return (
+	<form id="experience">
+		<h3>JOB EXPERIENCE</h3>
+		<h4><a>OJT: BULSU GRADUATE SCHOOL OFFICE</a>
+		<i>Helps the office sort the data they gathered from their students.</i></h4>
+		<h4><a>ANCHORS COLLECTION SERVICES INC</a>
+		<i>Worked as Telephone Collector</i></h4>
+		<h4><a>SYNTHESIS</a>
+		<i>Worked as Sports Analyst</i></h4>
+		
+		<h3>SCHOOL PROJECTS/FREELANCE PROJECTS</h3>
+		<h4><a>ONLINE GRADING PORTAL FOR ELEMENTARY AND HIGHSCHOOL IN ST. MARK'S COLLEGE</a>
+		<i>A website developed using PHP, AJAX, Javascript, HTML and CSS
+		Programmer</i></h4>
+		<h4><a>INFO NAVI: 2D ISOMETRIC NAVIGATION AND INFORMATION GUIDE FOR BULSU MAIN CAMPUS</a>
+		<i>Software Tester and Graphic Designer</i></h4>
+	</form>
+  )
+}
+ 
+export default Experience;
